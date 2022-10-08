36a8d350793bc570865f69b83f63b9617238d5ed
unknown <rizki.fadillah.20@student.is.ittelkom-sby.ac.id>
2022-10-08 22:25:41 +0700

    8.17

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './component/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* today's task */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's task</Text>
+      </View>
+      <View style={styles.items}>
+        {/* this is where the task will go */}
+        <Task text = {'Task 1'}/>
+        <Task text = {'Task 2'}/>
+      </View>
     </View>
   );
 }
@@ -13,8 +22,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  taskWrapper:{
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle:{
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items:{
+    paddingHorizontal: 20,
+    paddingTop: 2
+  }
+
 });
